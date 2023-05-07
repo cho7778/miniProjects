9fa9470e64a1f3db10dccede438dea1313cd6ad0
euncho.kang <107094445+cho7778@users.noreply.github.com>
2023-05-07 14:02:12 +0900

    feat: if문 수정

--- React-tic-tac-toe/my-app/src/App.js
+++ React-tic-tac-toe/my-app/src/App.js
@@ -67,7 +67,7 @@ function Board({ xIsNext, squares, onPlay }) {
 
 export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const [currentMove, setCurrentMove] = useState(0);
+  const [currentMove, setCurrentMove] = useState(0)
   const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
@@ -83,10 +83,10 @@ export default function Game() {
 
   const moves = history.map((squares, move) => {
     let description;
-    if (move > 0) {
-      description = 'Go to move #' + move;
-    } else {
+    if (!move) {
       description = 'Go to game start';
+    } else {
+      description = 'Go to move #' + move;
     }
     return (
       <li key={move}>
