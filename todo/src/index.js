0c5b434719b6fee6cc27385d37c9ee8eb9388c81
euncho.kang <wkdb0425@gmail.com>
2023-05-28 22:28:18 +0900

    chore: settings for MSW

--- todo/src/index.js
+++ todo/src/index.js
@@ -3,6 +3,11 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
+if (process.env.NODE_ENV === "development") {
+    const { worker } = require("./mocks/brower");
+    worker.start();
+}
+
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App />);
---
+++ todo/src/mocks/handlers.js
@@ -0,0 +1,119 @@
+import { rest } from "msw";
+
+const todos = {
+    title: "밥먹기",
+    isCompleted: false,
+    id: 1,
+    refId: [],
+    regTs: Date.now(),
+    updTs: null,
+};
+
+const todos1 = {
+    title: "씻기",
+    isCompleted: false,
+    id: 2,
+    refId: [],
+    regTs: Date.now(),
+    updTs: null,
+};
+
+const todos2 = {
+    title: "자기",
+    isCompleted: true,
+    id: 3,
+    refId: [],
+    regTs: Date.now(),
+    updTs: null,
+};
+
+const data = {
+    messages: [todos, todos1, todos2],
+};
+
+export const handlers = [
+    rest.get("/todos", (req, res, ctx) => {
+        return res(
+            ctx.status(200),
+            ctx.json({
+                messages: data.messages,
+            })
+        );
+    }),
+
+    rest.post("/todos", (req, res, ctx) => {
+        console.log("req", req);
+        console.log("res", res);
+        console.log("ctx", ctx);
+        const { id, title, isCompleted, refId, regTs } = JSON.parse(
+            req.body
+        )
+        const newRefId = refId.flatMap((id) =>
+            data.messages.filter((todo) => todo.id === id)
+        );
+        const newMessage = {
+            id: id,
+            title: title,
+            isCompleted: isCompleted,
+            refId: newRefId,
+            regTs: regTs,
+            updTs: null,
+        };
+
+        data.messages.push(newMessage);
+
+        return res(
+            ctx.status(200),
+            ctx.json({
+                resultType: "SUCCESSES",
+            })
+        );
+    }),
+
+    rest.put("/todos/:id", (req, res, ctx) => {
+        const { id } = req.params;
+
+        let body = req.body;
+        if (typeof req.body === "string") {
+            body = JSON.parse(req.body);
+        }
+
+        const { newTitle, isCompleted, refId } = body;
+
+        let todoToUpdate = data.messages.find((todo) => String(todo.id) === id);
+
+        if (todoToUpdate) {
+            if (refId && refId.length > 0) {
+                todoToUpdate.title = newTitle;
+                todoToUpdate.isCompleted = isCompleted;
+                todoToUpdate.refId = data.messages.filter((todo) =>
+                    refId.find((refTodo) => refTodo.id === todo.id)
+                );
+                todoToUpdate.updTs = Date.now();
+            } else {
+                todoToUpdate.title = newTitle;
+                todoToUpdate.isCompleted = isCompleted;
+                todoToUpdate.refId = refId;
+                todoToUpdate.updTs = Date.now();
+            }
+        }
+
+        return res(
+            ctx.status(200),
+            ctx.json({
+                resultType: "SUCCESSES",
+            })
+        );
+    }),
+
+    rest.delete("/todos/:id", (req, res, ctx) => {
+        const { id } = req.params;
+        data.messages = data.messages.filter((todo) => String(todo.id) !== id);
+        return res(
+            ctx.status(200),
+            ctx.json({
+                resultType: "SUCESSES",
+            })
+        );
+    }),
+];
